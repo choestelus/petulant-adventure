93a7c12d94dca45c46f6d4e52504f31c62f37a30
Choestelus <sprintf.null@gmail.com>
2015-04-19 17:18:00 +0700

    Init Project

---
+++ service.c
@@ -0,0 +1,63 @@
+#include "rfos.h"
+#include <stdio.h>
+
+static gboolean on_handle_get (
+    RFOS *object,
+    GDBusMethodInvocation *invocation,
+    const gchar *key,
+    const gchar *outpath) {
+
+    /** Your Code for Get method here **/
+    guint err = 0;
+    /** End of Get method execution, returning values **/
+    rfos_complete_get(object, invocation, err);
+    return TRUE;
+}
+
+static gboolean on_handle_put (
+    RFOS *object,
+    GDBusMethodInvocation *invocation,
+    const gchar *key,
+    const gchar *src) {
+
+    /** Your code for Put method here **/
+    guint err = 0;
+    /** End of Put method execution, returning values **/
+    rfos_complete_put(object, invocation, err);
+ 
+    return TRUE;
+}
+
+static void on_name_acquired (GDBusConnection *connection,
+    const gchar *name,
+    gpointer user_data)
+{
+    /* Create a new RFOS service object */
+    RFOS *skeleton = rfos_skeleton_new ();
+    /* Bind method invocation signals with the appropriate function calls */
+    g_signal_connect (skeleton, "handle-get", G_CALLBACK (on_handle_get), NULL);
+    g_signal_connect (skeleton, "handle-put", G_CALLBACK (on_handle_put), NULL);
+    /* Export the RFOS service on the connection as /kmitl/ce/os/RFOS object  */
+    g_dbus_interface_skeleton_export (G_DBUS_INTERFACE_SKELETON (skeleton),
+        connection,
+        "/kmitl/ce/os/RFOS",
+        NULL);
+}
+
+int main (void)
+{
+    /* Initialize daemon main loop */
+    GMainLoop *loop = g_main_loop_new (NULL, FALSE);
+    /* Attempt to register the daemon with 'kmitl.ce.os.RFOS' name on the bus */
+    g_bus_own_name (G_BUS_TYPE_SESSION,
+        "kmitl.ce.os.RFOS",
+        G_BUS_NAME_OWNER_FLAGS_NONE,
+        NULL,
+        on_name_acquired,
+        NULL,
+        NULL,
+        NULL);
+    /* Start the main loop */
+    g_main_loop_run (loop);
+    return 0;
+}
