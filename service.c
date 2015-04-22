27286916850c4b5705cc8228549bf918470ee4f2
Choestelus <sprintf.null@gmail.com>
2015-04-22 20:39:07 +0700

    properly read/write partial disk metadata

--- service.c
+++ service.c
@@ -1,5 +1,10 @@
 #include "rfos.h"
 #include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+
+const char magic_number[] = "e1e1";
+const int block_size = 64;
 
 char ***disk_array;
 int disk_number;
@@ -47,10 +52,43 @@ static void on_name_acquired (GDBusConnection *connection,
         NULL);
 }
 
+void init_disk(const char *disk_name, int tag)
+{
+    FILE *fp;
+    fp = fopen(disk_name, "rb+");
+    if(fp == NULL)
+    {
+        fprintf(stderr, "fopen failed with errno %d\n", errno);
+        exit(errno);
+    }
+
+    gint64 disk_size = 0;
+    fseek(fp, 0, SEEK_END);
+    disk_size = (gint64)ftello(fp);
+    fprintf(stderr, "%s %ld\n", "size = ", disk_size);
+    fprintf(stderr, "sizeof(gint64) = %lu sizeof(disk_size) = %lu\n", sizeof(gint64), sizeof(disk_size));
+
+    rewind(fp);
+
+    fwrite(magic_number, sizeof(char), sizeof(magic_number)/sizeof(char), fp);
+    fwrite(&disk_size, sizeof(gint64), sizeof(disk_size)/sizeof(gint64), fp);
+
+    fflush(fp);
+
+    fclose(fp);
+
+    fprintf(stderr, "%s\n", "disk initialized");
+}
+
 int main (int argc, char** argv)
 {
     disk_number = argc;
     disk_array = &argv;
+    int i;
+    for(i=1; i < argc; i++)
+    {
+        init_disk(argv[i], 0);
+    }
 
     /* Initialize daemon main loop */
     GMainLoop *loop = g_main_loop_new (NULL, FALSE);
